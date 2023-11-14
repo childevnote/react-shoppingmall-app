10a5584ef62fa3f4baf7d33d5e4a7e9763d39821
childevnote <daniel011125@gmail.com>
2023-11-14 22:22:36 +0900

    [Update] api connect

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,70 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Product } from './types';
 import '../styles/Home.css'
 
 export default function Home() {
 
-  const [selectedValue, setSelectedValue] = React.useState('모두');
+  const [selectedValue, setSelectedValue] = useState('all');
+  const [products, setProducts] = useState<Product[]>([]);
 
   const handleChange = (e: any) => {
     setSelectedValue(e.target.value);
-    console.log(e.target.value)
+    console.log(e.target.value);
   };
+
+  const getCategoryUrl = (category: string): string => {
+    return category === 'all' ? 'https://fakestoreapi.com/products' : `https://fakestoreapi.com/products/category/${category}`;
+  };
+
+  const renderButton = (category: string) => (
+    <button
+      value={category}
+      onClick={handleChange}
+      style={{ backgroundColor: `${selectedValue === category ? 'gray' : ''}` }}
+    >
+      {category}
+    </button>
+  );
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const url = getCategoryUrl(selectedValue);
+        const response = await fetch(url);
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, [selectedValue]);
 
   return (
     <div>
       <h1>Products</h1>
       <div className="products-buttons">
-        <button value={'모두'} onClick={handleChange} style={{ backgroundColor: `${selectedValue === '모두' ? "gray" : ''}` }}>모두</button>
-        <button value={'전자기기'} onClick={handleChange} style={{ backgroundColor: `${selectedValue === '전자기기' ? "gray" : ''}` }}>전자기기</button>
-        <button value={'쥬얼리'} onClick={handleChange} style={{ backgroundColor: `${selectedValue === '쥬얼리' ? "gray" : ''}` }}>쥬얼리</button>
-        <button value={'남성의류'} onClick={handleChange} style={{ backgroundColor: `${selectedValue === '남성의류' ? "gray" : ''}` }}>남성의류</button>
-        <button value={'여성의류'} onClick={handleChange} style={{ backgroundColor: `${selectedValue === '여성의류' ? "gray" : ''}` }}>여성의류</button>
-
+        {renderButton('all')}
+        {renderButton('electronics')}
+        {renderButton('jewelery')}
+        {renderButton("men's clothing")}
+        {renderButton("women's clothing")}
+      </div>
+      <div className="product-list">
+        <h2>Product List</h2>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <img
+                src={product.image}
+                alt={product.title}
+              />
+              <h3>{product.title}</h3>
+            </li>
+          ))
+          }
+        </ul>
       </div>
     </div>
   )
