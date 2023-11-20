1d1321d0e5cd97b1524c393dc42445186ee7f006
childevnote <daniel011125@gmail.com>
2023-11-20 20:32:31 +0900

    [Update] header hidden when scrolling down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,29 @@
-import '../styles/Header.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/Header.css';
 
 export default function Header() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrolledDown = prevScrollPos < currentScrollPos;
+
+      setPrevScrollPos(currentScrollPos);
+
+      setVisible(!isScrolledDown || currentScrollPos < 10); // 스크롤이 최상단에 위치하면 항상 보이도록 설정
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <div className='header'>
+    <div className={`header ${visible ? 'visible' : 'hidden'}`}>
       <h2>Shop</h2>
       <div className="header-buttons">
         <button>Cart</button>
@@ -10,5 +31,5 @@ export default function Header() {
         <button>Logout</button>
       </div>
     </div>
-  )
+  );
 }
