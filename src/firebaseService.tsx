0ae0ead53b93dece43700f6cde3142dd9a7c8812
childevnote <daniel011125@gmail.com>
2023-11-27 22:42:31 +0900

    [Create] db connection

--- src/firebaseService.tsx
+++ src/firebaseService.tsx
@@ -1,5 +1,7 @@
-import {  User as FirebaseUser, GoogleAuthProvider, User, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
-import { auth } from './firebase-config';
+import { User as FirebaseUser, GoogleAuthProvider, User, onAuthStateChanged, signInWithPopup, signOut, signInWithEmailAndPassword as signInWithEmailAndPasswordFirebase, createUserWithEmailAndPassword as createUserWithEmailAndPasswordFirebase } from 'firebase/auth';
+import {  doc, setDoc, getDoc, collection } from 'firebase/firestore';
+import { auth, db } from './firebase-config';
+
 
 export const signInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
@@ -8,6 +10,26 @@ export const signInWithGoogle = async () => {
     return result.user;
   } catch (error) {
     console.error('Google 로그인 중 오류 발생:', error);
+    throw error;
+  }
+};
+
+export const signInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    const result = await signInWithEmailAndPasswordFirebase(auth, email, password);
+    return result.user;
+  } catch (error) {
+    console.error('이메일/패스워드 로그인 중 오류 발생:', error);
+    throw error;
+  }
+};
+
+export const createUserWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    const result = await createUserWithEmailAndPasswordFirebase(auth, email, password);
+    return result.user;
+  } catch (error) {
+    console.error('이메일/패스워드 회원가입 중 오류 발생:', error);
     throw error;
   }
 };
@@ -26,4 +48,30 @@ export const onAuthStateChangedListener = (callback: (user: User | null) => void
   return unsubscribe;
 };
 
-export type { FirebaseUser }; 
+export const addUserToFirestore = async (uid: string, userData: any) => {
+  try {
+    const usersCollection = collection(db, 'users');
+    const userRef = doc(usersCollection, uid);
+    await setDoc(userRef, userData);
+  } catch (error) {
+    console.error('Error adding user to Firestore:', error);
+  }
+};
+
+export const getUserDataFromFirestore = async (uid: string) => {
+  try {
+    const userDocRef = doc(db, 'users', uid);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (userDocSnapshot.exists()) {
+      return userDocSnapshot.data();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching user data from Firestore:', error);
+    throw error;
+  }
+};
+
+export type { FirebaseUser };
